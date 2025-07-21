a468f6a7c0520429b7bf5394c18c202e7d32f71f
Quincy Hardin <61283652+Q-Chan69@users.noreply.github.com>
2025-07-21 10:48:40 -0500

    Create description box in JS on home page

--- src/home-page.js
+++ src/home-page.js
@@ -18,4 +18,31 @@ export function loadHome() {
     logoDiv.appendChild(title);
 
     content.appendChild(logoDiv);
+
+    const bodyDiv = document.createElement("div");
+    bodyDiv.id = "body-div";
+
+    const bodyCard = document.createElement("div");
+    bodyCard.classList.add("body-card");
+
+    const discBox = document.createElement("div");
+    discBox.id = "main-description-box";
+
+    const disc1 = document.createElement("p");
+    disc1.textContent = "Great burgers, greater times";
+    discBox.appendChild(disc1);
+
+    const disc2 = document.createElement("p");
+    disc2.textContent = "Your neighborhood burger joint, serving you with a smile since 1959. Wether it's freshly made Spudsy burgers or crispy crinkle fries, we work hard to ensure you leave happy."
+    discBox.appendChild(disc2);
+
+    const burgImgHolder = document.createElement("img");
+    burgImgHolder.src = burgerImage;
+    burgImgHolder.id = "burger-pic";
+
+
+    bodyCard.appendChild(discBox);
+    bodyCard.appendChild(burgImgHolder);
+    bodyDiv.appendChild(bodyCard);
+    content.appendChild(bodyDiv);
 }
